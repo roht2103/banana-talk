23f197f1ce4bf3f7083b36911655c8eaa2ca3d3f
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2022-07-27 22:58:35 +0530

    taken input and output it

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let btnTranslate = document.querySelector("#btn-translate");
 let textInput = document.querySelector("#text-area");
+let outputDiv = document.querySelector("#output");
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-  console.log("clicked");
-  console.log(textInput.value);
+  outputDiv.innerText = "fdsddsfds " + textInput.value;
 });
