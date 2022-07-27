67e1389d9452cbe9134309002eefdf58e201cf4a
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2022-07-27 22:14:04 +0530

    taked input and console log it

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var btnTranslate = document.querySelector("#btn-translate");
+let btnTranslate = document.querySelector("#btn-translate");
+let textInput = document.querySelector("#text-area");
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
   console.log("clicked");
+  console.log(textInput.value);
 });
