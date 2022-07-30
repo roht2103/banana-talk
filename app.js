959471f3ac3f10b727804be42369a6e82175b293
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2022-07-30 18:12:07 +0530

    started from first

--- app.js
+++
@@ -1,7 +0,0 @@
-let btnTranslate = document.querySelector("#btn-translate");
-let textInput = document.querySelector("#text-area");
-let outputDiv = document.querySelector("#output");
-
-btnTranslate.addEventListener("click", function clickEventHandler() {
-  outputDiv.innerText = "fdsddsfds " + textInput.value;
-});
