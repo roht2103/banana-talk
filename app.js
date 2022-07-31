baffc842188416cb52831157da4d8e955f437386
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2022-07-31 19:01:42 +0530

    input output proccessing done

--- app.js
+++ app.js
@@ -1,26 +1,9 @@
-const btnTranslate = document.querySelector("#btn-translate");
-const outputDiv = document.querySelector("#output");
-const textInput = document.querySelector("#text-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
 
-const url = "https://api.funtranslations.com/translate/minion.json";
-function urlcreator(text) {
-  return url + "?text=" + text;
-}
-function errorHandler(error) {
-  console.log("error occured", error);
-  alert("server down, try after some time");
+function clickHandler() {
+  outputDiv.innerText = txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler);
-
-function clickHandler() {
-  var inputText = textInput.value;
-
-  fetch(urlcreator(inputText))
-    .then((response) => response.json())
-    .then((json) => {
-      var translatedText = json.contents.translated;
-      outputDiv.innerText = translatedText;
-    })
-    .catch(errorHandler);
-}
